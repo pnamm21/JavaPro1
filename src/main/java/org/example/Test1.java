6521ec4ff8df040583314e646dfc4361da48f9cf
shonmikl <mikhailegorov676@gmail.com>
2023-05-16 22:00:33 +0200

    commit

--- src/main/java/org/example/Test1.java
+++ src/main/java/org/example/Test1.java
@@ -1,7 +1,22 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Test1 {
+    private void getArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > 15) {
+                System.out.println("!!!");
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("!!!!!");
+        int[] array = {2, 4, 1, 2, 7};
+
+
+        for (int i = 0; i < 10; i++) {
+
+        }
     }
 }
